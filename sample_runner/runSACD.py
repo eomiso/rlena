c10ce4b4a51071c7c36a3385b87da31e2decf133
Uiseop Eom <eomiso@Uiseops-MacBook-Pro-2.local>
2021-08-11 15:11:06 +0900

    init SACD train and test

--- sample_runner/runSACD.py
+++ sample_runner/runSACD.py
@@ -1,4 +1,33 @@
-import envs.playground.pommerman
+import pommerman
+from pommerman import agents
+from algos import sac
+
+def main(): 
+    print(pommerman.REGISTRY)
+
+    # Create a set of agents (exactly four)
+    agent_list = [
+        agents.SimpleAgent(),
+        agents.RandomAgent(),
+        agents.SimpleAgent(),
+        agents.RandomAgent()
+    ]
+
+    env = pommerman.make('PommeFFACompetition-v0', agent_list)
+    
+    # Run the episodes just like OpenAI Gym
+    for i_episode in range(1):
+        state = env.reset()
+        done = False
+        while not done:
+            env.render()
+            actions = env.act(state)
+            state, reward, done, info = env.step(actions)
+        print('Episode {} finished'.format(i_episode))
+    env.close()
+if __name__ == '__main__':
+    main()
+
 
 if __name__ == '__main__':
-    print(envs.playground.pommerman)
+    main()
