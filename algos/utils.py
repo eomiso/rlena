b0a2c5b0ad6665c2c621777d440751b18cb863b9
Uiseop Eom <eomiso@Uiseops-MacBook-Pro-2.local>
2021-08-17 22:12:53 +0900

    Undo the changes made to algos/utils.py in eomiso/dev [Why] My own utils will be kept in the utils folder

--- algos/utils.py
+++ algos/utils.py
@@ -1,7 +1,12 @@
 from typing import Dict, List
+from collections import deque
+import pprint
+
 import numpy as np
 from pommerman.agents import BaseAgent
 from pommerman.envs.v0 import Pomme
+from pommerman.configs import team_competition_env
+from rl2.buffers.base import ReplayBuffer
 
 
 class CustomAgent(BaseAgent):
@@ -14,11 +19,6 @@ class CustomEnvWrapper(Pomme):
         super().__init__(**config['env_kwargs'])
         self.seed(0)
         agents = {}
-        if 'OneVsOne-v0' in config['env_id']:
-            self.one_vs_one = True
-        else:
-            self.one_vs_one = False
-
         for agent_id in range(4):
             agents[agent_id] = CustomAgent(config["agent"](
                 agent_id, config["game_type"]))
@@ -26,13 +26,8 @@ class CustomEnvWrapper(Pomme):
         self.set_init_game_state(None)
         view_range = 2 * self._agent_view_size + 1
         locational_shape = (5, view_range, view_range)
-
         additional_shape = (8, )
         self.observation_shape = (locational_shape, additional_shape)
-        self._trainee_agents = None
-
-    def set_trainee_agents(self, *args):
-        self._trainee_agents = list(args)
 
     def reset(self):
         obs = super().reset()
@@ -63,10 +58,7 @@ class CustomEnvWrapper(Pomme):
 
     def _preprocessing(self, obs: List[Dict], **kwargs) -> List[Dict]:
         out = []
-        for i, d in enumerate(obs):
-            if i not in self._trainee_agents:
-                out.append(d)
-                continue
+        for d in obs:
             custom_obs = {}
             keys = ['alive', 'game_type', 'game_env']
             _ = list(map(d.pop, keys))  # remove useless obs
@@ -74,8 +66,7 @@ class CustomEnvWrapper(Pomme):
             # Change enums into int
             d.update({'teammate': d.get('teammate').value})
             enemies = list(map(lambda x: x.value, d.get('enemies')))
-            if not self.one_vs_one:
-                enemies.remove(9)  # Remove dummy agent from enemies list
+            enemies.remove(9)  # Remove dummy agent from enemies list
             d.update({'enemies': enemies})
 
             # Gather infos
@@ -84,19 +75,28 @@ class CustomEnvWrapper(Pomme):
             for k, v in d.items():
                 if hasattr(v, 'shape'):
                     # Make border walls for locational obs
-                    # obs['board'] borders are represented as 2(= Rigid wall)
+                    # obs['board'] borders are represented as 1(= Rigid wall)
                     # else borders are filled with 0 values.
                     if k != 'board':
-                        for _ in range(self._agent_view_size):
-                            v = np.insert(v, (0, v.shape[0]), 0, axis=0)
-                            v = np.insert(v, (0, v.shape[1]), 0, axis=1)
+                        v = np.insert(v, (0, v.shape[0]), 0, axis=0)
+                        v = np.insert(v, (0, v.shape[1]), 0, axis=1)
+
+                        if not kwargs.setdefault('global_obs', False):
+                            for _ in range(self._agent_view_size - 1):
+                                v = np.insert(v, (0, v.shape[0]), 0, axis=0)
+                                v = np.insert(v, (0, v.shape[1]), 0, axis=1)
+
                     else:
-                        for _ in range(self._agent_view_size):
-                            v = np.insert(v, (0, v.shape[0]), 2, axis=0)
-                            v = np.insert(v, (0, v.shape[1]), 2, axis=1)
+                        v = np.insert(v, (0, v.shape[0]), 1, axis=0)
+                        v = np.insert(v, (0, v.shape[1]), 1, axis=1)
+
+                        if not kwargs.get('global_obs'):
+                            for _ in range(self._agent_view_size - 1):
+                                v = np.insert(v, (0, v.shape[0]), 1, axis=0)
+                                v = np.insert(v, (0, v.shape[1]), 1, axis=1)
 
                     # Cut views by centering the agent for localized observation
-                    if not kwargs.setdefault('global', False):
+                    if not kwargs.get('global_obs'):
                         pos = np.array(d.get('position'))
                         view_range = 2 * self._agent_view_size + 1
                         v = v[pos[0]:pos[0] + view_range,
