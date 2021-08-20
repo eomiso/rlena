ae00506131b3bcff27222c4d966f50cbf3604196
Uiseop Eom <ytrtef@gmail.com>
2021-08-20 15:48:32 +0000

    added more elements to config dictionary

--- sample_runner/trainSACD.py
+++ sample_runner/trainSACD.py
@@ -3,6 +3,7 @@ import numpy as np
 import argparse
 from datetime import datetime
 from pathlib import Path
+import json
 
 from algos.sac_discrete import SACAgentDISC, SACModelDISC
 from utils.wrapper import ConservativeEnvWrapper
@@ -11,6 +12,7 @@ from pommerman.configs import one_vs_one_env, team_competition_fast_env
 import pommerman.envs as envs
 from pommerman.agents import SimpleAgent
 from pommerman.characters import Bomber
+from pommerman import constants
 
 from rl2.buffers.base import ReplayBuffer
 from rl2.examples.temp_logger import Logger
@@ -50,34 +52,53 @@ if __name__ == "__main__":
     argparser.add_argument('--update_interval', type=int, default=5)
     argparser.add_argument('--max_step', type=int, default=1000)
     argparser.add_argument('--dir_name', type=str, default='')
+    argparser.add_argument('--empty_map', type=bool, default=False)
+    argparser.add_argument('--rand_until', type=int, default=1028)
+    argparser.add_argument('--eps', type=float, default=0.5)
     args = argparser.parse_args()
 
     args.device = 'cuda:{}'.format(args.cuda_device) if torch.cuda.is_available() else 'cpu'
 
-    time = datetime.now().strftime('%Y%b%d_%H_%M_%S')
+    if not args.dir_name:
+        dir_name = datetime.now().strftime('%Y%b%d_%H_%M_%S')
+    else:
+        dir_name = args.dir_name
     config = EasyDict({
         'gamma': 0.99,
         'n_agents': 2,
         'n_env': 1,
         'train_interval': args.train_interval,
-        'train_after': 5098,
+        'train_after': args.rand_until,
         'update_interval': args.update_interval,
-        'update_after': 5098,
-        'rand_until': 5098,
+        'update_after': args.rand_until,
+        'rand_until': args.rand_until,
         'save_interval': 1000,
-        'batch_size': 64,
+        'batch_size': 32,
         'max_step': args.max_step,
-        'random_until': 5098,
         'device': args.device,
         'render':True,
-        'render_interval': 100,
+        'render_interval': 50,
         'log_interval': 10, 
-        'eps': 0.5, # for decaying epsilon greedy
-        'log_dir': f'./sac-discrete/train/log/{time}',
-        'save_dir': f'./sac-discrete/train/ckpt/{time}'
+        'eps': args.eps, # for decaying epsilon greedy
+        'log_dir': f'./SACDexp/train/log/{dir_name}',
+        'save_dir': f'./SACDexp/train/ckpt/{dir_name}',
+        'lr_ac': 1e-4,
+        'lr_cr': 1e-4,
     })
 
+
+    Path(config.log_dir).mkdir(parents=True, exist_ok=True)
+    # save experiment conditions
+    with open(config.log_dir+'/readme.txt', 'w') as f:
+       f.write(json.dumps(config)) 
+    logger = Logger(name='TestSACD', args=config, log_dir=config.log_dir)
+
+    # set environment
+
     env_config = one_vs_one_env()
+    if args.empty_map:
+        env_config['env_kwargs']['num_rigid'] =10
+        env_config['env_kwargs']['num_wood'] =10
     env_config['env_kwargs']['agent_view_size'] = 4
     env_config['env_kwargs']['max_step'] = args.max_step
     env = ConservativeEnvWrapper(env_config)
@@ -91,6 +112,8 @@ if __name__ == "__main__":
                         discrete=True,
                         injection_shape=additional_shape,
                         preprocessor=obs_handler,
+                        lr_ac=config.lr_ac,
+                        lr_cr=config.lr_cr,
                         is_save=True,
                         device=config.device)
 
@@ -107,7 +130,7 @@ if __name__ == "__main__":
                                 buffer_kwargs=buffer_kwargs,
                                 save_dir=config.save_dir,
                                 eps = config.eps,
-                                rand_until=config.rand_util,
+                                rand_until=config.rand_until,
                                 # log_dir='sac_discrete/train/log',
                                 character=Bomber(0, env_config["game_type"]))
 
@@ -115,13 +138,10 @@ if __name__ == "__main__":
         0: trainee_agent,
         1: SimpleAgent(env_config['agent'](1, env_config["game_type"])),
     }
-
-    env.set_init_game_state(None)
+        
     env.set_agents(list(agents.values()))
     env.set_training_agents(0)
     env.seed(44)
-    Path(config.log_dir).mkdir(parents=True, exist_ok=True)
-    logger = Logger(name='TestSACD', args=config)
     worker = SACDworker(env,
                         agents=[trainee_agent],
                         n_agents=config.n_agents,
@@ -133,6 +153,6 @@ if __name__ == "__main__":
                         render=config.render,
                         render_interval=config.render_interval,
                         is_save= True,
-                        random_until=config.random_until)
+                        random_until=config.rand_until)
 
     worker.run()
