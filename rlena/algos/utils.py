6d3f52c2a22a74a46f94983b8d53c16ff8faa725
Uiseop Eom <ytrtef@gmail.com>
2021-10-21 12:27:28 +0900

    [add] added IBMWithNormalization to algos/utils.py

--- rlena/algos/utils.py
+++ rlena/algos/utils.py
@@ -234,3 +234,43 @@ class Logger:
 
     def _truncate(self, s):
         return s[:20] + '...' if len(s) > 23 else s
+
+
+
+import torch.nn.functional as F
+from rl2.models.torch.base import InjectiveBranchModel
+
+
+class IBMWithNormalization(InjectiveBranchModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.eps = np.finfo(np.float32).eps.item()
+
+    def foward(self, observation, injection, *args, **kwargs):
+        observation = self._handle_obs_shape(observation)
+        injection = injection.unsqueeze(0)
+        injection = F.normalize(injection, eps=self.eps)
+        ir = self.encoder(observation, *args, **kwargs)
+        if self.recurrent:
+            ir, hidden = ir
+        ir = torch.cat([ir, injection], dim=-1)
+        output = self.head(ir)
+
+        if self.recurrent:
+            return output, hidden
+        return output
+
+    def forward_trg(self, observation, injection, *args, **kwargs):
+        observation = self._handle_obs_shape(observation)
+        injection = F.normalize(injection, eps=self.eps)
+        with torch.no_grad():
+            ir = self.encoder_target(observation, *args)
+            if self.recurrent:
+                hidden = ir[1]
+                ir = ir[0]
+            ir = torch.cat([ir, injection], dim=-1)
+            output = self.head_target(ir)
+
+        if self.recurrent:
+            return output, hidden
+        return output
