89406391b30efd7f31f887c025788fbcd2801e53
Uiseop Eom <ytrtef@gmail.com>
2021-11-11 02:25:26 +0000

    setup.py version 0.0.1 -> 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rlena',
-    version='0.0.1',
+    version='0.1.0',
     description='rl agents on pommerman and marl_snake',
     url='https://gitlab.diyaml.com/rl2021/rlena',
     author='rl2021',
